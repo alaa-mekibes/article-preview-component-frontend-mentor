673af3616f58ad6d134dfb3e5f1522af0e707829
Alaa Mekibes <alaamks115@gmail.com>
2025-05-25 16:31:16 +0100

    first upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# Frontend Mentor - Article preview component solution
+
+This is a solution to the [Article preview component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- See the social media share links when they click the share icon
+
+### Screenshot
+
+![](/assets/images/screenshot.png)
+
+### Links
+
+- [Solution on Frontend Mentor](https://www.frontendmentor.io/solutions/interactive-rating-component-solution-TMRSojuUxv)
+- [live site URL](https://alaa-mekibes.github.io/article-preview-component-frontend-mentor)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JS
+- OOP
+- Mobile-first workflow
+
+### JS Methods
+
+```js
+class ShareBtn {
+  isDesktop(); // For checking if the device is desktop or mobile
+  init(); // For initiallising style if the user resieze the browser
+  btnClick(); // For Changing style if the user click on share button
+}
+```
---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+class ShareBtn {
+    constructor() {
+        this.btn = document.querySelector(".share-icon");
+        this.socialMedia = document.querySelector(".social-media");
+        this.profile = document.querySelector(".profile");
+        this.share = document.querySelector(".share");
+        this.shareBox = document.querySelector(".share-box");
+        this.init();
+        this.btnClick();
+    }
+    
+    isDesktop() {
+        return window.matchMedia('(min-width: 48rem)').matches;
+    }
+
+    init() {
+        window.addEventListener("resize", _ => {
+        if(!this.isDesktop()) {
+            this.profile.classList.remove("displayN");
+            this.share.classList.remove("share-mobile");
+            this.socialMedia.classList.remove("display");
+            this.shareBox.classList.remove("width-full");
+        }
+        else {
+            this.socialMedia.classList.remove("display");
+        }
+        })
+    }
+
+    btnClick() {
+        this.btn.addEventListener("click", _ => {
+            if(this.isDesktop()) {
+                this.socialMedia.classList.toggle("display");
+            }
+            else {
+                this.profile.classList.toggle("displayN");
+                this.share.classList.toggle("share-mobile");
+                this.socialMedia.classList.toggle("display");
+                this.shareBox.classList.toggle("width-full");
+            }
+        })
+
+        if(this.isDesktop()) {
+            document.addEventListener("click", (e) => {
+                if(!this.btn.contains(e.target) && !this.socialMedia.contains(e.target) )
+                    this.socialMedia.classList.remove("display");
+            });
+        }   
+    }
+}
+
+// main
+mybtn = new ShareBtn;
